11a2599f5f25c30fce59e82d20ee6d5145ff0bb6
pixcelo <bodomu@gmail.com>
2024-08-04 18:23:52 +0900

    add: チケットステータス

---
+++ WinFormsRedmine/Enums/TicketStatus.cs
@@ -0,0 +1,20 @@
+﻿namespace WinFormsRedmine.Enums
+{
+    /// <summary>
+    /// チケットのステータス
+    /// </summary>
+    public enum TicketStatus
+    {
+        New = 0,
+
+        Assined = 1,
+
+        Developing = 2,
+
+        Resolved = 3,
+
+        Closed = 4,
+
+        Pending = 5
+    }
+}
