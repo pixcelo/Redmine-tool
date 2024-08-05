575e23fa624b9888bfff97bce631a44c72178644
pixcelo <bodomu@gmail.com>
2024-08-06 08:17:19 +0900

    mod: グローバル例外ハンドラの設定

--- WinFormsRedmine/Program.cs
+++ WinFormsRedmine/Program.cs
@@ -10,7 +10,11 @@ namespace WinFormsRedmine
         /// </summary>
         [STAThread]
         static void Main()
-        {            
+        {
+            // グローバル例外ハンドラの設定
+            Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
             var services = new ServiceCollection();
             ConfigureServices(services);
 
@@ -27,6 +31,32 @@ namespace WinFormsRedmine
         {
             services.AddSingleton<MainForm>();            
             services.AddTransient<IApiAccessor, ApiAccessor>();
+        }
+
+        private static void Application_ThreadException(
+            object sender,
+            ThreadExceptionEventArgs e)
+        {
+            HandleException(e.Exception);
+        }
+
+        private static void CurrentDomain_UnhandledException(
+            object sender,
+            UnhandledExceptionEventArgs e)
+        {
+            if (e.ExceptionObject is Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+        
+        private static void HandleException(Exception ex)
+        {
+            MessageBox.Show(
+                $"Error: {ex.Message}, {ex.StackTrace}",
+                "Error",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
         }
     }
 }
