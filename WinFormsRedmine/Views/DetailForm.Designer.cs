6d68bbf7b25a539e9322954cf07c60a827daaf74
tetsu.k <bodomu@gmail.com>
2024-08-08 06:48:02 +0900

    add: 詳細画面

---
+++ WinFormsRedmine/Views/DetailForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WinFormsRedmine.Views
+{
+    partial class DetailForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            descriptionTextBox = new TextBox();
+            subjectTextBox = new TextBox();
+            copyButton = new Button();
+            SuspendLayout();
+            // 
+            // descriptionTextBox
+            // 
+            descriptionTextBox.Location = new Point(73, 192);
+            descriptionTextBox.Multiline = true;
+            descriptionTextBox.Name = "descriptionTextBox";
+            descriptionTextBox.ScrollBars = ScrollBars.Vertical;
+            descriptionTextBox.Size = new Size(540, 338);
+            descriptionTextBox.TabIndex = 0;
+            // 
+            // subjectTextBox
+            // 
+            subjectTextBox.Location = new Point(73, 39);
+            subjectTextBox.Name = "subjectTextBox";
+            subjectTextBox.Size = new Size(540, 23);
+            subjectTextBox.TabIndex = 1;
+            // 
+            // copyButton
+            // 
+            copyButton.Location = new Point(619, 38);
+            copyButton.Name = "copyButton";
+            copyButton.Size = new Size(75, 23);
+            copyButton.TabIndex = 2;
+            copyButton.Text = "copy";
+            copyButton.UseVisualStyleBackColor = true;
+            copyButton.Click += copyButton_Click;
+            // 
+            // DetailForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 560);
+            Controls.Add(copyButton);
+            Controls.Add(subjectTextBox);
+            Controls.Add(descriptionTextBox);
+            Name = "DetailForm";
+            Text = "DetailForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox descriptionTextBox;
+        private TextBox subjectTextBox;
+        private Button copyButton;
+    }
+}
---
+++ WinFormsRedmine/Views/DetailForm.cs
@@ -0,0 +1,45 @@
+﻿using WinFormsRedmine.Models;
+
+namespace WinFormsRedmine.Views
+{
+    public partial class DetailForm : Form
+    {
+        private readonly IssueViewModel viewModel;
+
+        public DetailForm(Issue issue) : this()
+        {
+            this.viewModel = new IssueViewModel(issue);
+
+            this.SetUp();
+        }
+
+        public DetailForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void SetUp()
+        {
+            this.subjectTextBox.Text = $"{this.viewModel.Id} {this.viewModel.Subject}";
+
+            //this.idLabel.Text = this.viewModel.Id.ToString();
+            //this.subjectLabel.Text = this.viewModel.Subject;
+            //this.trackerLabel.Text = this.viewModel.TrackerName;
+            //this.statusLabel.Text = this.viewModel.StatusName;
+            //this.assignedToLabel.Text = this.viewModel.AssignedName;
+            //this.fixedVersionLabel.Text = this.viewModel.FixedVersion;
+            this.descriptionTextBox.Text = this.viewModel.Description;
+            //this.customFieldsTextBox.Text = this.viewModel.CustomFields;
+        }
+
+        /// <summary>
+        /// クリップボードにコピーする
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void copyButton_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(this.subjectTextBox.Text);
+        }
+    }
+}
