3cc9c4f456a8006c4e7df97635b4a364698fb5f8
tetsu.k <bodomu@gmail.com>
2024-10-15 06:28:34 +0900

    [mod] ユーザーシークレットからAPIキーとURLを取得してアクセスする

--- WinFormsRedmine/Classes/ApiAccessor.cs
+++ WinFormsRedmine/Classes/ApiAccessor.cs
@@ -1,18 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using WinFormsRedmine.Models;
 
 namespace WinFormsRedmine.Classes
 {
+    /// <summary>
+    /// APIアクセサ
+    /// </summary>
     public interface IApiAccessor
     {
+        /// <summary>
+        /// イシュー一覧を取得する
+        /// </summary>
+        /// <param name="issueRequest"></param>
+        /// <see href="https://www.redmine.org/projects/redmine/wiki/Rest_api#google_vignette"/>
+        /// <returns></returns>
+        Task<List<Issue>> FetchIssues(IssueRequest issueRequest);
 
+        /// <summary>
+        /// イシューを取得する
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<Issue?> FetchIssue(string id);
     }
 
     public sealed class ApiAccessor : IApiAccessor
     {
+        
+        private static readonly HttpClient httpClient = new HttpClient();
+        private readonly string? baseUrl;
+        private readonly string? apiKey;
+
+        public ApiAccessor()
+        {
+            var configurationRoot = new ConfigurationBuilder()
+                .AddUserSecrets("e397cc54-ecd1-4af6-953c-c638a5aa028b")
+                .Build();
+
+            this.baseUrl = configurationRoot["BaseUrl"];
+            this.apiKey = configurationRoot["ApiKey"];
+        }
+
+        public async Task<List<Issue>> FetchIssues(IssueRequest issueRequest)
+        {
+            var url = $"{this.baseUrl}/redmine/issues.json";                
+            var requestUrl = $"{url}?assigned_to_id=me&key={this.apiKey}";
+
+            HttpResponseMessage response = await httpClient.GetAsync(requestUrl);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            var issueResponse = JsonConvert.DeserializeObject<IssueResponse>(responseBody);
+            return issueResponse?.Issues;
+        }
+
+        public async Task<Issue?> FetchIssue(string id)
+        {
+            var url = $"{this.baseUrl}/redmine/issues/{id}.json";
+            var requestUrl = $"{url}?assigned_to_id=me&key={apiKey}";
+
+            HttpResponseMessage response = await httpClient.GetAsync(requestUrl);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            var issueResponse = JsonConvert.DeserializeObject<IssueResponseSingle>(responseBody);
+            return issueResponse?.Issue;
+        }
 
     }
 }
